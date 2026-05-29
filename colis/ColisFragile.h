51f328f1e4652e373990f2119403132d3e27b09e
dak-kaw <kawtar.dakir.67@edu.uiz.ac.ma>
2026-05-29 11:01:44 +0000

    Update ColisFragile.h
    -> estFragile() retourne toujours true

--- colis/ColisFragile.h
+++ colis/ColisFragile.h
@@ -3,10 +3,12 @@
 
 class ColisFragile : public Colis
 {
+private: 
+    int niveauFragilite;
+
 public:
-    ColisFragile(float pd, float lo, float la, float h);
+    ColisFragile(float pd, float lo, float la, float h, int niveau = 1);
 
     void afficher() const override;
-
-    bool estFragile() const;
+    int getNiveau() const;
 };
