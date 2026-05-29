2aa0cbecc753fe433ad94b0dd77514d3e29a15ab
dak-kaw <kawtar.dakir.67@edu.uiz.ac.ma>
2026-05-29 12:20:34 +0000

    Update Transporteur.h
    évite l'inclusion circulaire entre Transporteur.h et Colis.h

--- Transporteur.h
+++ Transporteur.h
@@ -2,22 +2,22 @@
 #define TRANSPORTEUR_H
 
 #include <string>
-using namespace std;
 
 class Colis;
 
 class Transporteur {
 protected:
-    string nom;
+    std::string nom;
 
 public:
-    Transporteur(string n);
+    Transporteur(std::string n);
 
     virtual bool canDeliver(const Colis& c) const = 0;
     virtual double computeCost(const Colis& c) const = 0;
     virtual double computeDelay(const Colis& c) const = 0;
 
-    string getName() const;
+    std::string getNom() const;
+    virtual void afficher() const;
 
     virtual ~Transporteur();
 };
