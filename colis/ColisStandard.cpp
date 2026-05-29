5ec1e91f577cf31d9ce186c8173fe3669fecbf36
dak-kaw <kawtar.dakir.67@edu.uiz.ac.ma>
2026-05-29 11:15:20 +0000

    Update ColisStandard.cpp

--- colis/ColisStandard.cpp
+++ colis/ColisStandard.cpp
@@ -2,12 +2,21 @@
 #include <iostream>
 using namespace std;
 
-ColisStandard::ColisStandard() : Colis() {}
+ColisStandard::ColisStandard()
+    : Colis(), prioritaire(false) {}
 
-ColisStandard::ColisStandard(float pd, float lo, float la, float h)
-    : Colis(pd, lo, la, h, TypeColis::standard) {}
+ColisStandard::ColisStandard(float pd, float lo, float la, float h, boll prio)
+    : Colis(pd, lo, la, h, TypeColis::Standard),
+    prioritaire(prio)
+{}
+
+bool ColisStandard::isPrioritaire() const {
+    return prioritaire;
+}
 
 void ColisStandard::afficher() const {
-    cout << "=== Colis Standard ===" << endl;
+    cout << "=== Colis Standard 
+        << (prioritaire ? " [PRIORITAIRE]" : "")
+        << "===\n";
     cout << *this;
 }
