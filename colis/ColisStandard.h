2eb7012fa2d54564a33b5218994ba442f7fc7220
dak-kaw <kawtar.dakir.67@edu.uiz.ac.ma>
2026-05-29 11:10:59 +0000

    Update ColisStandard.h

--- colis/ColisStandard.h
+++ colis/ColisStandard.h
@@ -2,9 +2,12 @@
 #include "Colis.h"
 
 class ColisStandard : public Colis {
+private:
+    bool prioritaire;
 public:
     ColisStandard();
-    ColisStandard(float pd, float lo, float la, float h);
+    ColisStandard(float pd, float lo, float la, float h, bool prio = false);
 
     void afficher() const override;
+    bool isPrioritaire() const;
 };
